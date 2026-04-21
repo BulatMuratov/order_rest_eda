bc7258a60fe58152675585955ac7358342c1de5b
 Bulat <bulat@MacBook-Air-Bulat.local>
2026-04-22 01:09:17 +0300

    infrastructure services

---
+++ config-service/src/main/java/com/bulka/ConfigApplication.java
@@ -0,0 +1,16 @@
+package com.bulka;
+
+import me.paulschwarz.springdotenv.DotenvConfig;
+import me.paulschwarz.springdotenv.DotenvPropertySource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
